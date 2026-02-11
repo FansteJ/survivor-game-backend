f97367d84e9013c3e4066be0e902e6bdbd95dde6
Stefan Josipovic <stefan.josipovic2005@gmail.com>
2026-02-11 16:33:47 +0100

    chore: Initialize Spring Boot project

---
+++ src/main/java/com/stefan/survivorgamebackend/SurvivorGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stefan.survivorgamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivorGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivorGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefan/survivorgamebackend/SurvivorGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefan.survivorgamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivorGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
