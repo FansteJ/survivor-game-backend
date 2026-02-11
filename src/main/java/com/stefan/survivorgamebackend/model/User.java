691becc28da7324d86aee16163040999d927e9ac
Stefan Josipovic <stefan.josipovic2005@gmail.com>
2026-02-11 22:03:57 +0100

    feat: add User entity

---
+++ src/main/java/com/stefan/survivorgamebackend/model/User.java
@@ -0,0 +1,33 @@
+package com.stefan.survivorgamebackend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, nullable = false)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
