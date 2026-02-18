aaee73222f865fad464b4f58353a210f56e3ee80
Stefan Josipovic <stefan.josipovic2005@gmail.com>
2026-02-18 01:24:57 +0100

    feat: add GameController with startGame and finishGame endpoints

---
+++ src/main/java/com/stefan/survivorgamebackend/controller/GameController.java
@@ -0,0 +1,34 @@
+package com.stefan.survivorgamebackend.controller;
+
+import com.stefan.survivorgamebackend.dto.FinishGameSessionRequest;
+import com.stefan.survivorgamebackend.model.UserProfile;
+import com.stefan.survivorgamebackend.service.FinishGameSessionService;
+import com.stefan.survivorgamebackend.service.StartGameSessionService;
+import com.stefan.survivorgamebackend.service.UserProfileService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/game")
+@AllArgsConstructor
+public class GameController {
+    private final StartGameSessionService startGameSessionService;
+    private final FinishGameSessionService finishGameSessionService;
+
+    @PostMapping("/start")
+    public ResponseEntity<UUID> startGame() {
+        return ResponseEntity.ok(startGameSessionService.startGameSession());
+    }
+
+    @PostMapping("/finish")
+    public ResponseEntity<?> finishGame(@RequestBody FinishGameSessionRequest finishGameSessionRequest) {
+        finishGameSessionService.finishGameSession(finishGameSessionRequest);
+        return ResponseEntity.ok().build();
+    }
+}
